c8f6c1c3f1bda742f7ae5ffa31d83b5cc372fbe2
Adelino <adelinolsn@gmail.com>
2019-03-18 16:33:44 -0300

    Melhorias de layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,9 +1,7 @@
 @extends('layouts.principal')
-
+@section('title','Início')
+@section('path','Início')
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
             <div class="card">
                 <div class="card-header">Dashboard</div>
 
--- resources/views/layouts/body.blade.php
+++ resources/views/layouts/body.blade.php
@@ -1,3 +1,9 @@
 <section class="content">
-    @yield('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                @yield('content')
+            </div>
+        </div>
+    </div>
 </section>
