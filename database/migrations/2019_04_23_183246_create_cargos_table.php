1e4c71aec59b1527db899ce9b2a58951aaf6a8bf
AdelinoLSN <adelinolsn@gmail.com>
2019-04-23 16:08:58 -0300

    Adicionado Seeder de Cargo

--- database/migrations/2019_04_23_183246_create_cargos_table.php
+++ database/migrations/2019_04_23_183246_create_cargos_table.php
@@ -15,6 +15,9 @@ class CreateCargosTable extends Migration
     {
         Schema::create('cargos', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('nome');
+            $table->string('especializacao')->nullable();
+            
             $table->timestamps();
         });
     }
