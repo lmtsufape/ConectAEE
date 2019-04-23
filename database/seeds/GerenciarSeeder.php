364867fe4d16009b9f61856df7be0af08983cfba
AdelinoLSN <adelinolsn@gmail.com>
2019-04-23 16:57:13 -0300

    Adicionado Seeder de Gerenciar

---
+++ database/seeds/GerenciarSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Gerenciar;
+use App\Aluno;
+use App\User;
+
+class GerenciarSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $alunos = Aluno::all();
+        $users_count = count(User::all());
+
+        foreach ($alunos as $aluno) {
+            factory(Gerenciar::class)->create([
+                'aluno_id' => $aluno->id,
+                'cargo_id' => rand(1,4),
+                'user_id' => rand(1,$users_count),
+            ]);
+        }
+        
+        //factory(Gerenciar::class, count(Aluno::all()))->create();
+    }
+}
