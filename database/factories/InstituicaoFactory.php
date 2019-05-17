ae087a77fa6f29e96d1873a8848cf8f8083774d6
PC da UAG <pc@uag.ufrpe.br>
2019-05-17 16:42:57 -0300

    Adicionado migration, factory e seeder de escola

---
+++ database/factories/InstituicaoFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Instituicao;
+use Illuminate\Support\Str;
+use Faker\Generator as Faker;
+
+/*
+|--------------------------------------------------------------------------
+| Model Factories
+|--------------------------------------------------------------------------
+|
+| This directory should contain each of the model factory definitions for
+| your application. Factories provide a convenient way to generate new
+| model instances for testing / seeding your application's database.
+|
+*/
+
+$factory->define(Instituicao::class, function (Faker $faker) {
+    return [
+        'nome' => $faker->company,
+        'telefone' => $faker->phoneNumber,
+        'email' => $faker->companyEmail,
+        'endereco' => $faker->streetAddress
+    ];
+});
