f367420dbbaf61115b9202ed4dd43780feea3088
EbersonSantos <eberson.santos1@gmail.com>
2019-10-12 01:29:15 -0300

    atualizado edicao de sugestao

--- resources/views/sugestao/editar.blade.php
+++ resources/views/sugestao/editar.blade.php
@@ -13,68 +13,79 @@
   <div class="row">
     <div class="col-md-12">
       <div class="panel panel-default">
-        <div class="panel-heading">Editar Sugestao</div>
+        <div class="panel-heading">
+          <h2>
+            <strong>
+              Editar Sugestão
+            </strong>
+          </h2>
 
-        <div class="panel-body">
-          <form class="form-horizontal" method="POST" action="{{ route("objetivo.sugestao.atualizar") }}">
-            {{ csrf_field() }}
-
-            <input type="hidden" name="id_sugestao" value="{{ $sugestao->id }}">
-
-            <div class="form-group{{ $errors->has('titulo') ? ' has-error' : '' }}">
-              <label for="titulo" class="col-md-4 control-label">Título <font color="red">*</font> </label>
-
-              <div class="col-md-6">
-                @if(old('titulo',NULL) != NULL)
-                  <input id="titulo" type="text" class="form-control" name="titulo" value="{{ old('titulo') }}" autofocus>
-                @else
-                  <input id="titulo" type="text" class="form-control" name="titulo" value="{{ $sugestao->titulo }}" autofocus>
-                @endif
-
-                @if ($errors->has('titulo'))
-                  <span class="help-block">
-                    <strong>{{ $errors->first('titulo') }}</strong>
-                  </span>
-                @endif
-              </div>
-            </div>
-
-            <div class="form-group{{ $errors->has('descricao') ? ' has-error' : '' }}">
-              <label for="descricao" class="col-md-4 control-label">Descrição</label>
-
-              <div class="col-md-6">
-
-                @if(old('descricao',NULL) != NULL)
-                <textarea id="descricao" rows = "5" cols = "50" class="form-control" name="descricao">{{ old('descricao') }}</textarea>
-                @else
-                <textarea id="descricao" rows = "5" cols = "50" class="form-control" name="descricao">{{ $sugestao->descricao }}</textarea>
-                @endif
-
-                @if ($errors->has('descricao'))
-                <span class="help-block">
-                  <strong>{{ $errors->first('descricao') }}</strong>
-                </span>
-                @endif
-              </div>
-            </div>
-
-            <div class="form-group">
-              <div class="col-md-6 col-md-offset-4">
-                <button type="submit" class="btn btn-success">
-                  Atualizar
-                </button>
-              </div>
-            </div>
-          </form>
+          <hr style="border-top: 1px solid black;">
         </div>
 
-        <div class="panel-footer">
+        <div class="panel-body panel-body-cadastro">
+          <div class="col-md-8 col-md-offset-2">
+            <form method="POST" action="{{ route("objetivo.sugestao.atualizar") }}">
+              {{ csrf_field() }}
+
+              <input type="hidden" name="id_sugestao" value="{{ $sugestao->id }}">
+
+              <div class="form-group{{ $errors->has('titulo') ? ' has-error' : '' }}">
+                <label for="titulo" class="col-md-12 control-label">Título <font color="red">*</font> </label>
+
+                <div class="col-md-12">
+                  @if(old('titulo',NULL) != NULL)
+                    <input id="titulo" type="text" class="form-control" name="titulo" value="{{ old('titulo') }}" autofocus>
+                  @else
+                    <input id="titulo" type="text" class="form-control" name="titulo" value="{{ $sugestao->titulo }}" autofocus>
+                  @endif
+
+                  @if ($errors->has('titulo'))
+                    <span class="help-block">
+                      <strong>{{ $errors->first('titulo') }}</strong>
+                    </span>
+                  @endif
+                </div>
+              </div>
+
+              <div class="form-group{{ $errors->has('descricao') ? ' has-error' : '' }}">
+                <label for="descricao" class="col-md-12 control-label">Descrição</label>
+
+                <div class="col-md-12">
+
+                  @if(old('descricao',NULL) != NULL)
+                  <textarea id="descricao" rows = "5" cols = "50" class="form-control" name="descricao">{{ old('descricao') }}</textarea>
+                  @else
+                  <textarea id="descricao" rows = "5" cols = "50" class="form-control" name="descricao">{{ $sugestao->descricao }}</textarea>
+                  @endif
+
+                  @if ($errors->has('descricao'))
+                  <span class="help-block">
+                    <strong>{{ $errors->first('descricao') }}</strong>
+                  </span>
+                  @endif
+                </div>
+              </div>
+
+              <div class="form-group">
+                <div class="row col-md-12 text-center">
+                  <br>
+                  <button type="submit" class="btn btn-primary">
+                    Atualizar
+                  </button>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+
+        <!-- <div class="panel-footer">
           <a class="btn btn-danger" href="{{URL::previous()}}">
             <i class="material-icons">keyboard_backspace</i>
             <br>
             Voltar
           </a>
-        </div>
+        </div> -->
       </div>
     </div>
   </div>
