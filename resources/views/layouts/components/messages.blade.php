9818862101952c3b2f484d6599595b363be76e6b
Douglas Filipe <batista.douglasfs@gmail.com>
2025-03-13 19:26:45 -0300

    fix: correção no cadastro de usuários no sistema, inserção do beginTransaction =, commit e rollback

--- resources/views/layouts/components/messages.blade.php
+++ resources/views/layouts/components/messages.blade.php
@@ -4,9 +4,9 @@
         <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
     </div>
 @endif
-@if(session('fail'))
+@if(session('error'))
     <div class="alert alert-danger d-flex justify-content-between align-items-center">
-        <span class="mx-auto">{{ session('fail') }}</span>
+        <span class="mx-auto">{{ session('error') }}</span>
         <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
     </div>
 @endif
